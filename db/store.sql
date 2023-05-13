b9226c2e247187b96071ebaee6c735f16be52461
mackliu <macklun@gmail.com>
2023-05-13 16:05:54 +0800

    更新資料庫內容

--- db/store.sql
+++ db/store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2023-05-13 04:55:24
+-- 產生時間： 2023-05-13 10:05:26
 -- 伺服器版本： 10.4.27-MariaDB
 -- PHP 版本： 8.2.0
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫： `store2`
+-- 資料庫： `store`
 --
 
 -- --------------------------------------------------------
@@ -32,6 +32,14 @@ CREATE TABLE `customs` (
   `created_time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- 傾印資料表的資料 `customs`
+--
+
+INSERT INTO `customs` (`id`, `created_time`) VALUES
+(1, '2023-05-13 07:59:59'),
+(2, '2023-05-13 08:04:09');
+
 -- --------------------------------------------------------
 
 --
@@ -41,9 +49,22 @@ CREATE TABLE `customs` (
 CREATE TABLE `orders` (
   `id` int(10) UNSIGNED NOT NULL,
   `product_id` int(10) UNSIGNED NOT NULL,
-  `custom_id` int(10) UNSIGNED ZEROFILL NOT NULL,
+  `custom_id` int(10) UNSIGNED NOT NULL,
   `created_time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `orders`
+--
+
+INSERT INTO `orders` (`id`, `product_id`, `custom_id`, `created_time`) VALUES
+(1, 1, 1, '2023-05-13 07:59:59'),
+(2, 6, 1, '2023-05-13 07:59:59'),
+(3, 12, 1, '2023-05-13 07:59:59'),
+(4, 5, 2, '2023-05-13 08:04:09'),
+(5, 6, 2, '2023-05-13 08:04:09'),
+(6, 10, 2, '2023-05-13 08:04:09'),
+(7, 11, 2, '2023-05-13 08:04:09');
 
 -- --------------------------------------------------------
 
@@ -70,11 +91,10 @@ INSERT INTO `products` (`id`, `name`, `price`, `up`, `created_time`, `updated_ti
 (4, '火腿三明治', 25, 1, '2023-05-13 01:44:54', '2023-05-13 01:44:54'),
 (5, '紅茶(中)', 20, 1, '2023-05-13 01:48:12', '2023-05-13 01:48:12'),
 (6, '肉鬆蛋吐司', 30, 1, '2023-05-13 01:48:12', '2023-05-13 01:48:12'),
-(7, '漢堡蛋', 35, 1, '2023-05-13 01:48:12', '2023-05-13 01:48:12'),
-(8, '雞腿堡', 40, 1, '2023-05-13 01:48:12', '2023-05-13 01:48:12'),
 (10, '蘿蔔糕', 25, 1, '2023-05-13 01:48:12', '2023-05-13 01:48:12'),
 (11, '總匯三明治', 50, 1, '2023-05-13 01:55:08', '2023-05-13 01:55:08'),
-(12, '柳澄汁', 50, 1, '2023-05-13 01:55:08', '2023-05-13 01:55:08');
+(12, '柳澄汁', 50, 1, '2023-05-13 01:55:08', '2023-05-13 01:55:08'),
+(13, '漢堡蛋', 35, 1, '2023-05-13 07:15:56', '2023-05-13 07:15:56');
 
 --
 -- 已傾印資料表的索引
@@ -106,19 +126,19 @@ ALTER TABLE `products`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `customs`
 --
 ALTER TABLE `customs`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
